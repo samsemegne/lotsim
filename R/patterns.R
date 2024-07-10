57059843aa63e72f5be510b43dc1008d845a6143
unknown <samsemegne@gmail.com>
2024-07-10 11:37:30 +0200

    Fixes rnorm_runs()

--- R/patterns.R
+++ R/patterns.R
@@ -73,8 +73,9 @@ rnorm_runs = function(x, sigma, min_run, max_run, prob_run) {
   vals = numeric()
   while (length(vals) < len) {
     remaining_len = len - length(vals)
-    run_len = rbinom(1L, max_run, prob_run) |>
-      lotsim::clamp(min_run, remaining_len)
+    run_len = rbinom(1L, max_run, prob_run)#|>
+    run_len = max(run_len, min_run_len, na.rm = FALSE)
+    run_len = min(run_len, remaining_len, na.rm = FALSE)
 
     val = rnorm(1L, 0L, sigma)
     vals = c(vals, rep(val, run_len))
